375b63ce55f015b09ceba8dc8fa24ecb70fbf652
Joel Refuerzo <joelandrie123@gmail.com>
2025-07-08 19:17:11 +0800

    feat: update project name and repository links in package.json and cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -6,37 +6,37 @@ const fs = require('fs');
 
 const projectName = process.argv[2];
 if (!projectName) {
-  console.error('Please provide a name for your project.');
-  process.exit(1);
+    console.error('Please provide a name for your project.');
+    process.exit(1);
 }
 
-const gitRepo = 'https://github.com/Leoj030/express-project-boilerplate.git';
+const gitRepo = 'https://github.com/Leoj030/create-express-ts-project.git';
 const projectPath = path.join(process.cwd(), projectName);
 
 try {
-  console.log('Downloading files...');
-  execSync(`git clone --depth 1 ${gitRepo} ${projectPath}`);
+    console.log('Downloading files...');
+    execSync(`git clone --depth 1 ${gitRepo} "${projectPath}"`);
 
-  process.chdir(projectPath);
+    process.chdir(projectPath);
 
-  console.log('Installing dependencies...');
-  execSync('npm install');
+    console.log('Installing dependencies...');
+    execSync('npm install');
 
-  console.log('Removing unnecessary files...');
-  execSync('npx rimraf ./.git');
-  // Assuming 'bin' is part of the boilerplate and needs to be removed
-  if (fs.existsSync(path.join(projectPath, 'bin'))) {
-    fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true });
-  }
+    console.log('Removing unnecessary files...');
+    execSync('npx rimraf ./.git');
   
-  console.log('\nInstallation is done! The project is ready to use.');
-  console.log('\nTo start the development server with auto-reload, run:');
-  console.log(`  cd ${projectName}`);
-  console.log('  npm run dev:start');
+    if (fs.existsSync(path.join(projectPath, 'bin'))) {
+        fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true });
+    }
+  
+    console.log('\nInstallation is done! The project is ready to use.');
+    console.log('\nTo start the development server with auto-reload, run:');
+    console.log(`  cd ${projectName}`);
+    console.log('  npm run dev:start');
 
-  console.log('\nTo only check for type errors without running the server, you can run:');
-  console.log('  npm run dev:type-check');
+    console.log('\nTo only check for type errors without running the server, you can run:');
+    console.log('  npm run dev:type-check');
 
 } catch (error) {
-  console.error(`An error occurred: ${error.message}`);
+    console.error(`An error occurred: ${error.message}`);
 }
